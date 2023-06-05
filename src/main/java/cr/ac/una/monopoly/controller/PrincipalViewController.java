327f25a019687fdaf21ba4e3b04fee297593575f
Joshua Cambronero <129781505+EJ-Djarin@users.noreply.github.com>
2023-06-05 10:41:51 -0600

    Add files via upload

--- src/main/java/cr/ac/una/monopoly/controller/PrincipalViewController.java
+++ src/main/java/cr/ac/una/monopoly/controller/PrincipalViewController.java
@@ -36,10 +36,14 @@ public class PrincipalViewController extends Controller implements Initializable
     private JFXTextField txfJugador2;
     @FXML
     private JFXTextField txfJugador1;
+    @FXML
+    private ImageView ImVJugador1;
+    @FXML
+    private ImageView ImVJugador2;
 
    public void nombreJugadores(String nombre1,String nombre2){
        lblName1.setText(nombre1);
-          lblName2.setText(nombre2);
+       lblName2.setText(nombre2);
     }
     @FXML
     private JFXButton btnFinalizarJuego;
@@ -79,8 +83,8 @@ public class PrincipalViewController extends Controller implements Initializable
 
     private int valDados= 0;
     private int valTotal=0;
-       private int sumaTotal1=0;
-         private int sumaTotal2=0;
+    private int sumaTotal1=0;
+    private int sumaTotal2=0;
     private int comodin;
     
     private int fila;
@@ -88,8 +92,13 @@ public class PrincipalViewController extends Controller implements Initializable
     private int columna;
     private int c;
     private int f;
-    private Image fichaJugador1=new Image("/cr/ac/una/monopoly/resources/helicoptero.png");
-    private Image fichaJugador2=new Image("/cr/ac/una/monopoly/resources/sombrero.png");
+
+    private String imageJ1;
+    private String imageJ2;
+    
+    
+   // private Image fichaJugador1=new Image("/cr/ac/una/monopoly/resources/helicoptero.png");
+  //  private Image fichaJugador2=new Image("/cr/ac/una/monopoly/resources/sombrero.png");
     @FXML
     void onActionBtnFinalizarJuego(ActionEvent event) {
         FlowController.getInstance().salir();
@@ -190,33 +199,51 @@ void mostrarImagenDado(int dado, ImageView imageView) {
 
     }
     private boolean fichaJugador2Creada = false;
-private int jugadorActual = 1;
+    private int jugadorActual = 1;
+
     @FXML
     void onActionBtnTerminarTurno(ActionEvent event) {
         imgDado1.setImage(new Image("/cr/ac/una/monopoly/resources/fondoDado.png"));
         imgDado2.setImage(new Image("/cr/ac/una/monopoly/resources/fondoDado.png"));
         btnLanzarDados.setDisable(false);
         jugadorActual = (jugadorActual == 1) ? 2 : 1;
-    if (!fichaJugador2Creada) {
-        Circle ficha2 = crearFicha(Color.BLUE, fichaJugador2);
-        fichaJugador2Creada = true;
+        if (!fichaJugador2Creada) {
+            Circle ficha2 = crearFicha(Color.BLUE, imageJ2);
+            fichaJugador2Creada = true;
+        }
     }
-    }
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    Datos datos = (Datos) AppContext.getInstance().get("Llave");
-    lblName1.setText(datos.getJugador1());
-    lblName2.setText(datos.getJugador2());
+        imageJ1 = "/cr/ac/una/monopoly/resources/circuloAzul.png";
+        imageJ2 = "/cr/ac/una/monopoly/resources/circuloVerde.png";
+
+        Datos datos = (Datos) AppContext.getInstance().get("Llave");
+        lblName1.setText(datos.getJugador1());
+        lblName2.setText(datos.getJugador2());
+        Datos rut = (Datos) AppContext.getInstance().get("Ruta");
+
+        if (rut != null) {
+            if (rut.getRuta1() != null) {
+                System.out.println("Esta es la ruta1 que jala: " + rut.getRuta1());
+                System.out.println("Esta es la ruta2 que jala: " + rut.getRuta2());
+                imageJ1 = rut.getRuta1();
+                imageJ2 = rut.getRuta2();
+
+            }
+        }
+
+        Image imageB = new Image(imageJ1);
+        ImVJugador1.setImage(imageB);
+        Image imageA = new Image(imageJ2);
+        ImVJugador2.setImage(imageA);
+
+        Circle ficha1 = crearFicha(Color.GREEN, imageJ1);
+        initPositionMap();
+    }  
     
-    Circle ficha1 = crearFicha(Color.GREEN, fichaJugador1);
-        
- 
-   initPositionMap();
-    }    
     @Override
     public void initialize() {
     }
@@ -225,91 +252,86 @@ private int jugadorActual = 1;
     private Map<Integer, Position> positionMap;
 
 // Clase auxiliar para representar una posición en el GridPane
-private class Position {
-    private int row;
-    private int column;
+    private class Position {
 
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
+        private int row;
+        private int column;
 
-    public int getRow() {
-        return row;
-    }
+        public Position(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
 
-    public int getColumn() {
-        return column;
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
     }
-}
 
 // Método de inicialización para crear el mapeo de posiciones
-private void initPositionMap() {
-            positionMap = new HashMap<>();
-    positionMap.put(0, new Position(8, 8));
-    positionMap.put(1, new Position(8, 7));
-    positionMap.put(2, new Position(8, 6));
-    positionMap.put(3, new Position(8, 5));
-    positionMap.put(4, new Position(8, 4));
-    positionMap.put(5, new Position(8, 3));
-    positionMap.put(6, new Position(8, 2));
-    positionMap.put(7, new Position(8, 1));
-    positionMap.put(8, new Position(8, 0));
-    positionMap.put(9, new Position(7, 0));
-    positionMap.put(10, new Position(6, 0));
-    positionMap.put(11, new Position(5, 0));
-    positionMap.put(12, new Position(4, 0));
-    positionMap.put(13, new Position(3, 0));
-    positionMap.put(14, new Position(2, 0));
-    positionMap.put(15, new Position(1, 0));
-    positionMap.put(16, new Position(0, 0));
-    positionMap.put(17, new Position(0, 1));
-    positionMap.put(18, new Position(0, 2));
-    positionMap.put(19, new Position(0, 3));
-    positionMap.put(20, new Position(0, 4));
-    positionMap.put(21, new Position(0, 5));
-    positionMap.put(22, new Position(0, 6));
-    positionMap.put(23, new Position(0, 7));
-    positionMap.put(24, new Position(0, 8));
-    positionMap.put(25, new Position(1, 8));
-    positionMap.put(26, new Position(2, 8));
-    positionMap.put(27, new Position(3, 8));
-    positionMap.put(28, new Position(4, 8));
-    positionMap.put(29, new Position(5, 8));
-    positionMap.put(30, new Position(6, 8));
-    positionMap.put(31, new Position(7, 8));
+    private void initPositionMap() {
+        positionMap = new HashMap<>();
+        positionMap.put(0, new Position(8, 8));
+        positionMap.put(1, new Position(8, 7));
+        positionMap.put(2, new Position(8, 6));
+        positionMap.put(3, new Position(8, 5));
+        positionMap.put(4, new Position(8, 4));
+        positionMap.put(5, new Position(8, 3));
+        positionMap.put(6, new Position(8, 2));
+        positionMap.put(7, new Position(8, 1));
+        positionMap.put(8, new Position(8, 0));
+        positionMap.put(9, new Position(7, 0));
+        positionMap.put(10, new Position(6, 0));
+        positionMap.put(11, new Position(5, 0));
+        positionMap.put(12, new Position(4, 0));
+        positionMap.put(13, new Position(3, 0));
+        positionMap.put(14, new Position(2, 0));
+        positionMap.put(15, new Position(1, 0));
+        positionMap.put(16, new Position(0, 0));
+        positionMap.put(17, new Position(0, 1));
+        positionMap.put(18, new Position(0, 2));
+        positionMap.put(19, new Position(0, 3));
+        positionMap.put(20, new Position(0, 4));
+        positionMap.put(21, new Position(0, 5));
+        positionMap.put(22, new Position(0, 6));
+        positionMap.put(23, new Position(0, 7));
+        positionMap.put(24, new Position(0, 8));
+        positionMap.put(25, new Position(1, 8));
+        positionMap.put(26, new Position(2, 8));
+        positionMap.put(27, new Position(3, 8));
+        positionMap.put(28, new Position(4, 8));
+        positionMap.put(29, new Position(5, 8));
+        positionMap.put(30, new Position(6, 8));
+        positionMap.put(31, new Position(7, 8));
 
+    }
+    private Circle crearFicha(Color color, String fichaImage) {
+        Circle ficha = new Circle(14);
+        ficha.setFill(color);
+        ImageView fichaImageView = new ImageView(fichaImage);
+        fichaImageView.setFitWidth(50);
+        fichaImageView.setFitHeight(50);
+        gridPaneTablero.add(fichaImageView, 8, 8);
 
-}
-private Circle crearFicha(Color color, Image fichaImage) {
-    Circle ficha = new Circle(14);
-    ficha.setFill(color);
-    ImageView fichaImageView = new ImageView(fichaImage);
-    fichaImageView.setFitWidth(50);
-    fichaImageView.setFitHeight(50);
-    gridPaneTablero.add(fichaImageView, 8, 8);
+        fichaImageView.setOnMouseClicked(event -> {
+            if ((jugadorActual == 1 && color == Color.GREEN) || (jugadorActual == 2 && color == Color.BLUE)) {
+                c = gridPaneTablero.getColumnIndex(fichaImageView);
+                f = gridPaneTablero.getRowIndex(fichaImageView);
+                int comodin = 0;
 
-    fichaImageView.setOnMouseClicked(event -> {
-        if ((jugadorActual == 1 && color == Color.GREEN) || (jugadorActual == 2 && color == Color.BLUE)) {
-            c = gridPaneTablero.getColumnIndex(fichaImageView);
-            f = gridPaneTablero.getRowIndex(fichaImageView);
-            int comodin = 0;
+                Position position = positionMap.get(valTotal);
+                if (position != null) {
+                    columna = position.getColumn();
+                    fila = position.getRow();
+                }
+                gridPaneTablero.setColumnIndex(fichaImageView, columna);
+                gridPaneTablero.setRowIndex(fichaImageView, fila);
+            }
+        });
 
-            Position position = positionMap.get(valTotal);
-            if (position != null) {
-                columna = position.getColumn();
-                fila = position.getRow();
-            } 
-
-            gridPaneTablero.setColumnIndex(ficha, columna);
-            gridPaneTablero.setRowIndex(ficha, fila);
-            gridPaneTablero.setColumnIndex(fichaImageView, columna);
-            gridPaneTablero.setRowIndex(fichaImageView, fila);
-        }
-    });
-    
-    txfJugador2.setText("C:" + c + " y F:" + f);
-    
-    return ficha;
-}
+        return ficha;
+    }
 }
